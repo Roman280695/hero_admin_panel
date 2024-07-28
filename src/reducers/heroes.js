54e8230591651d713391c95b25be9e6ec3ac5d2c
Roman_Petrov <you@example.com>
2024-07-28 19:03:15 +0300

    first commit

---
+++ src/reducers/heroes.js
@@ -0,0 +1,42 @@
+const initialState = {
+    heroes: [],
+    heroesLoadingStatus: 'idle'
+}
+
+const heroes = (state = initialState, action) => {
+    switch (action.type) {
+        case 'HEROES_FETCHING':
+            return {
+                ...state,
+                heroesLoadingStatus: 'loading'
+            }
+        case 'HEROES_FETCHED':
+            return {
+                ...state,
+                heroes: action.payload,
+                heroesLoadingStatus: 'idle'
+            }
+        case 'HEROES_FETCHING_ERROR':
+            return {
+                ...state,
+                heroesLoadingStatus: 'error'
+            }
+        // Самая сложная часть - это показывать новые элементы по фильтрам
+        // при создании или удалении
+        case 'HERO_CREATED':
+            // Формируем новый массив    
+            return {
+                ...state,
+                heroes: [...state.heroes, action.payload]
+            }
+        case 'HERO_DELETED': 
+            // Формируем новый массив
+            return {
+                ...state,
+                heroes: state.heroes.filter(item => item.id !== action.payload)
+            }
+        default: return state
+    }
+}
+
+export default heroes;
